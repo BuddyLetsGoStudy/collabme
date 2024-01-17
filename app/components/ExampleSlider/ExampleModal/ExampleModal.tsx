6d07b311a835ea5b59136039599cdd95db86aa92
Nikita <nikita@logachev.top>
2024-01-17 07:35:31 +0300

    in

---
+++ app/components/ExampleSlider/ExampleModal/ExampleModal.tsx
@@ -0,0 +1,25 @@
+import styles from "./ExampleModal.module.scss";
+
+export const ExampleModal = ({ title, description, keys }: any) => {
+  return (
+    <div className={styles.background}>
+      <div className={styles.modal}>
+        <div className={styles.title}>{title}</div>
+        <div className={styles.content}>
+          <div className={styles.video}></div>
+          <div className={styles.text}>
+            <div className={styles.textTitle}>Текст из видео</div>
+            <div className={styles.description}>{description}</div>
+            <div className={styles.keysTitle}>Динамичные переменные</div>
+            <div className={styles.keys}>
+              {keys.map((key: any) => (
+                <div className={styles.keysBlock}>#{key}</div>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className={styles.cross}></div>
+      </div>
+    </div>
+  );
+};
